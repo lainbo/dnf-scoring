e82c359f49eb5afcdd31d5e26c5316c78ce83928
liangjiacheng <lainbo@foxmail.com>
2022-09-19 13:35:23 +0800

    feat: 轮询鉴权

--- src/App.vue
+++ src/App.vue
@@ -271,11 +271,19 @@ export default {
     if (LOCAL_BOTTOM_INFO) {
       this.bottomInfo = LOCAL_BOTTOM_INFO
     }
-    this.getAuth()
+    this.polling()
   },
   mounted () {},
   methods: {
+    // 轮询获取权限
+    polling () {
+      this.getAuth()
+      setInterval(() => {
+        this.getAuth()
+      }, 1000 * 60 * 8)
+    },
     async getAuth () {
+      console.log(new Date().getTime())
       const url = 'https://www.fastmock.site/mock/003e6703669b7e59c74e8460e6fc0100/tools/scoreboard/auth'
       const { data } = await axios.post(url)
       this.disabled = data.disabled
